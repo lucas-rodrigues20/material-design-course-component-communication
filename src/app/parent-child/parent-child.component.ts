1643fccc172239afb3731c5940fd7cea83a9301c
Lucas Rodrigues Moura <lucas.rodrigues20@outlook.com>
2019-09-29 23:03:49 -0300

    ViewChild

--- src/app/parent-child/parent-child.component.ts
+++ src/app/parent-child/parent-child.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { TimerComponent } from './timer/timer.component';
 
 @Component({
   selector: 'app-parent-child',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ParentChildComponent implements OnInit {
 
+  @ViewChild("stopwatch2", { static: true })
+  private myTimer: TimerComponent;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  start() {
+    this.myTimer.start();
+  }
+
+  stop() {
+    this.myTimer.stop();
+  }
+
+  clear() {
+    this.myTimer.clear();
   }
 
 }
