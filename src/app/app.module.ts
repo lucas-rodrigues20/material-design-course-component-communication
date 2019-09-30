0dc83b851a3a908aae15471b69bf54e3c9871840
Lucas Rodrigues Moura <lucas.rodrigues20@outlook.com>
2019-09-29 21:44:17 -0300

    Gerando Eventos Com o EventEmmiter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,28 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { InputBindingComponent } from './input-binding/input-binding.component';
 import { ClientComponent } from './input-binding/client/client.component';
+import { EventComponent } from './event/event.component';
+import { ChildItemComponent } from './event/child-item/child-item.component';
+import { 
+  MatCardModule, 
+  MatButtonModule, 
+  MatIconModule 
+} from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent,
     InputBindingComponent,
-    ClientComponent
+    ClientComponent,
+    EventComponent,
+    ChildItemComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
