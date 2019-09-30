7f942a6e1bc23732a71a31a46e1f3ae98e77abdf
Lucas Rodrigues Moura <lucas.rodrigues20@outlook.com>
2019-09-29 21:19:01 -0300

    Input Binding com Objetos

--- src/app/input-binding/input-binding.component.ts
+++ src/app/input-binding/input-binding.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Client } from './client.model';
 
 @Component({
   selector: 'app-input-binding',
@@ -7,10 +8,20 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class InputBindingComponent implements OnInit {
 
-  @Input() name: string;
-  @Input('othername') lastName: string;
+  @Input('firstName') name: string;
+  @Input() lastName: string;
+  @Input() age: number;
 
-  constructor() { }
+  clients: Client[];
+
+  constructor() {
+    this.clients = [
+      { id: 1, name: "Client 01", age: 20 },
+      { id: 2, name: "Client 02", age: 21 },
+      { id: 3, name: "Client 03", age: 23 },
+      { id: 4, name: "Client 04", age: 22 }
+    ];
+  }
 
   ngOnInit() {
   }
